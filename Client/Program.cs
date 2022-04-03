d187724a08d84b339720ace66b56a635b42debd8
Michal Paukert <paukert.michal@gmail.com>
2022-04-03 19:59:36 +0200

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+TcpClient tcpClient = new TcpClient();
+var serverIp = IPAddress.Parse("192.168.1.115");
+int port = 13000;
+tcpClient.SendBufferSize = 8192;
+Console.WriteLine("Connecting to server");
+tcpClient.Connect(serverIp, port);
+Console.WriteLine("Connected");
+
+var rnd = new Random();
+var networkStream = tcpClient.GetStream();
+
+byte[] bytes = new byte[1073741824]; // 1 GB
+rnd.NextBytes(bytes);
+
+networkStream.Write(bytes, 0, bytes.Length);
+
+
+
+networkStream.Close();
---
+++ Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+int port = 13000;
+
+var server = new TcpListener(IPAddress.Any, port);
+
+// Start listening for client requests.
+server.Start();
+
+// Buffer for reading data
+string data = null;
+
+while (true)
+{
+    Console.Write("Waiting for a connection... ");
+
+    TcpClient tcpClient = server.AcceptTcpClient();
+    var ip = tcpClient.Client.RemoteEndPoint as IPEndPoint;
+    Console.WriteLine("Connected!", ip.Address);
+
+    // Get a stream object for reading and writing
+    NetworkStream stream = tcpClient.GetStream();
+    int i;
+    byte[] bytes = new byte[1073741824]; // 1 GB
+
+    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+    {
+
+    }
+
+    tcpClient.Close();
+}
